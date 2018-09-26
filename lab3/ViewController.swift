57a206221f0bba31106f8c0fa251a4f6a1ab9e7c
Enyinnaya Asonye <enyinnayaasonye@Enyinnayas-MacBook-Pro.local>
2018-09-25 19:16:30 -0500

    yesterday and today steps working

--- lab3/ViewController.swift
+++ lab3/ViewController.swift
@@ -11,10 +11,12 @@ import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var prevSteps: UILabel!
+    @IBOutlet weak var todaySteps: UILabel!
     let activityManager = CMMotionActivityManager()
     let customQueue = OperationQueue()
     let pedometer = CMPedometer()
-    
+    let startOfDay = NSCalendar.current.startOfDay(for: Date())
    
     
     override func viewDidLoad() {
@@ -23,37 +25,39 @@ class ViewController: UIViewController {
         if CMMotionActivityManager.isActivityAvailable(){
             self.activityManager.startActivityUpdates(to: customQueue)
             { (activity:CMMotionActivity?) -> Void in
-                NSLog("%@", activity!.description)
+//                print("%@", activity!.description)
             }
             
         }
         
         if CMPedometer.isStepCountingAvailable(){
-            pedometer.startUpdates(from: Date())
-            {(pedData: CMPedometerData?, error: Error?) -> Void in
-                print("%@", pedData?.description)
-            }
+            //calculates number of steps real time
+            pedometer.startUpdates(from: startOfDay, withHandler: {(data,error) in
+                if let pedData = data {
+                    DispatchQueue.main.async {
+                        self.todaySteps.text = "\(pedData.numberOfSteps)"
+                    }
+                }
+
+            })
         }
+            //this causes pedometer to stop counting steps immediately, should be used elsewhere or not at all
+//        if CMPedometer.isStepCountingAvailable(){
+//            self.pedometer.stopUpdates()
+//        }
         
-        if CMPedometer.isStepCountingAvailable(){
-            self.pedometer.stopUpdates()
-        }
-        
-        let now = Date()
+        let now = NSDate()
         let from = now.addingTimeInterval(-60*60*24)
         
-        self.pedometer.queryPedometerData(from: from, to: now as Date)
+        self.pedometer.queryPedometerData(from: from as Date, to: now as Date)
         {(pedData: CMPedometerData?, error: Error?) -> Void in
             if let unwrappedPedData = pedData{
-            let aggr_string = """
-            Steps: \(unwrappedPedData.numberOfSteps) \n
-            Distance \(unwrappedPedData.distance)
-                Floors: \(unwrappedPedData.floorsAscended?.intValue)
-            """
-            
-            DispatchQueue.main.async(){
-                 // self.activityLabel.text = aggr_string
-            }
+                let aggr_string = """
+                \(unwrappedPedData.numberOfSteps)
+                """
+                DispatchQueue.main.async(){
+                    self.prevSteps.text = aggr_string;
+                }
             
             }
         }
