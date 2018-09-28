44255ebf19c9a07c3ac3cb3e2034b26f0d279f4b
Brandon McFarland <branmcf@gmail.com>
2018-09-28 00:34:04 -0500

    Added v1 of game. Need to meet with Enyi to add exceptional credit.

---
+++ lab3/GameViewController.swift
@@ -0,0 +1,38 @@
+//
+//  GameViewController.swift
+//  lab3
+//
+//  Created by Brandon McFarland on 9/26/18.
+//  Copyright © 2018 Saminator5. All rights reserved.
+//
+
+//import Foundation
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //setup game scene
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView // the view in storyboard must be an SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+    }
+    
+    
+    
+    override var prefersStatusBarHidden : Bool {
+        return true
+    }
+    
+    
+    
+}
